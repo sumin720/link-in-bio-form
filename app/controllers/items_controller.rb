512b5de89ee2d1ba9297f52e285611376f2a3537
sumin720 <sthoma11@chicagobooth.edu>
2024-11-18 18:43:22 +0000

    the hulk

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,19 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+  def new_item_form
+    render({ :template => "item_templates/new_item_form" })
+  end
+
+  def create_new_item
+    new_item = Item.new
+
+    new_item.link_url = params.fetch("query_link_url")
+    new_item.link_description = params.fetch("query_link_description")
+    new_item.thumbnail_url = params.fetch("query_thumbnail_url")
+
+    new_item.save
+
+    redirect_to("/")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+  get("/backdoor", { :controller => "items", :action => "new_item_form" })
+  post("/insert_item", { :controller => "items", :action => "create_new_item" })
+  
 end
